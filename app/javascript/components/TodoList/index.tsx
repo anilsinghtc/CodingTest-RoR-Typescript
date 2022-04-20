65a75862dd13a42ab7d339fa63cee1bf9e163ff2
Anil <anil01tc@gmail.com>
2022-04-20 13:37:14 +0000

    ror-typescript bug fixes

--- app/javascript/components/TodoList/index.tsx
+++ app/javascript/components/TodoList/index.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, ListGroup, Form } from "react-bootstrap";
 import { ResetButton } from "./uiComponent";
 import axios from "axios";
 
 type TodoItem = {
-  id: number;
-  title: string;
-  checked: boolean;
+  id: number,
+  title: string,
+  checked: boolean
 };
 
 type Props = {
@@ -14,12 +14,16 @@ type Props = {
 };
 
 const TodoList: React.FC<Props> = ({ todoItems }) => {
+  
+  const [data, setData] = useState([])
   useEffect(() => {
     const token = document.querySelector(
       "[name=csrf-token]"
     ) as HTMLMetaElement;
     axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
-  }, []);
+
+setData(todoItems)
+ }, []);
 
   const checkBoxOnCheck = (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -28,18 +32,27 @@ const TodoList: React.FC<Props> = ({ todoItems }) => {
     axios.post("/todo", {
       id: todoItemId,
       checked: e.target.checked,
-    });
+    })
+  todoItems.map(todo=>{
+    if(todo.id === todoItemId){
+      todo.checked = !todo.checked
+    }
+  })
+  let data = [...todoItems]
+   setData(data)
   };
 
   const resetButtonOnClick = (): void => {
-    axios.post("/reset").then(() => location.reload());
+    axios.post("/reset").then(() => {
+    window.location.reload()
+    });
   };
 
   return (
     <Container>
       <h3>2022 Wish List</h3>
       <ListGroup>
-        {todoItems.map((todo) => (
+        {data.map((todo) => (
           <ListGroup.Item key={todo.id}>
             <Form.Check
               type="checkbox"
